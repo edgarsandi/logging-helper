c14703eeaf5c7548d4a4705da4d214edfd321e96
Edgar R. Sandi <edgar.r.sandi@gmail.com>
2015-09-10 09:04:55 -0300

    Removed unnecessary code

--- src/Log/Formatter/LogstashFormatter.php
+++ src/Log/Formatter/LogstashFormatter.php
@@ -6,12 +6,6 @@ class LogstashFormatter extends \Monolog\Formatter\LogstashFormatter
 {
     public function __construct($applicationName, $systemName = null, $extraPrefix = null, $contextPrefix = 'ctxt_')
     {
-        parent::__construct('Y-m-d\TH:i:s.uP');
-
-        $this->systemName = $systemName ?: gethostname();
-        $this->applicationName = $applicationName;
-        $this->extraPrefix = $extraPrefix;
-        $this->contextPrefix = $contextPrefix;
-        $this->version = 1;
+        parent::__construct($applicationName, $systemName = null, $extraPrefix = null, $contextPrefix = 'ctxt_', $version = self::V1);
     }
 }
